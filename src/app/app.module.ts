5f2d5d1b7f70a9a50a75f93cbad4e9f9ed4b40d0
paulabenites <paula.beni.tes@hotmail.com>
2020-09-23 22:26:43 -0500

    C2 - W3 Spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
@@ -64,6 +65,7 @@ import { LoginComponent } from './login/login.component'
     MatInputModule,
     MatSlideToggleModule,
     MatSelectModule,
+    MatProgressSpinnerModule,
     FormsModule,
     ReactiveFormsModule
   ],
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { resolve } from 'url';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
@@ -9,16 +10,26 @@ export class DishService {
 
   constructor() { }
   getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);  
+    return new Promise(
+      resolve => {
+        //Simula latencia de server (2 sg)
+        setTimeout(() => resolve(DISHES), 2000)
+      });  
   }
 
   getDish(id: string):Promise<Dish>{
-    return Promise.resolve(DISHES.filter((dish) => (dish.id == id) )[0]);
+
+    return new Promise(
+      resolve => {
+        setTimeout(() => resolve(DISHES.filter((dish) => (dish.id == id) )[0]), 2000)
+      });
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => (dish.featured))[0]);
-
+    return new Promise(
+      resolve => {
+        setTimeout(() => resolve(DISHES.filter((dish) => (dish.featured) )[0]), 2000)
+      });
   }
 
 }
